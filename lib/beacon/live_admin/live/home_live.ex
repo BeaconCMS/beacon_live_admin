adfafdfb6cdb89c4d0646bf13eec6c7395290aca
Leandro Pereira <leandro@leandro.io>
2023-07-27 17:31:41 -0400

    remove dark styles

--- lib/beacon/live_admin/live/home_live.ex
+++ lib/beacon/live_admin/live/home_live.ex
@@ -29,25 +29,25 @@ defmodule Beacon.LiveAdmin.HomeLive do
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <%= for site <- @running_sites do %>
             <div class="max-w-sm p-6 mt-10 rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><%= site %></h5>
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><%= site %></h5>
 
               <.link
                 href={Beacon.LiveAdmin.Router.beacon_live_admin_path(@socket, site, "/layouts")}
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
               >
                 Layouts
               </.link>
 
               <.link
                 href={Beacon.LiveAdmin.Router.beacon_live_admin_path(@socket, site, "/pages")}
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
               >
                 Pages
               </.link>
 
               <.link
                 href={Beacon.LiveAdmin.Router.beacon_live_admin_path(@socket, site, "/media_library")}
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
               >
                 Media Library
               </.link>
