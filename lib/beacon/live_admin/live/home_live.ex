9d42e2b244684c008684813b33a28a02a5e52990
Leandro Pereira <leandro@leandro.io>
2023-08-14 14:14:52 -0400

    add Components button to home

--- lib/beacon/live_admin/live/home_live.ex
+++ lib/beacon/live_admin/live/home_live.ex
@@ -46,6 +46,13 @@ defmodule Beacon.LiveAdmin.HomeLive do
               </.link>
 
               <.link
+                href={Beacon.LiveAdmin.Router.beacon_live_admin_path(@socket, site, "/components")}
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Components
+              </.link>
+
+              <.link
                 href={Beacon.LiveAdmin.Router.beacon_live_admin_path(@socket, site, "/media_library")}
                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
               >
