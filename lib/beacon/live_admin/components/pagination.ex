b6b715cd2ffbc21f726ac5cbad694ec67a4fe1db
ChristianTovar <tovarchristian21@gmail.com>
2023-12-06 12:22:38 -0500

    Add basic styling

--- lib/beacon/live_admin/components/pagination.ex
+++ lib/beacon/live_admin/components/pagination.ex
@@ -3,12 +3,10 @@ defmodule Beacon.LiveAdmin.Components.Pagination do
 
   def render(assigns) do
     ~H"""
-    <div>
-      <span>prev</span>
-      <span :for={page <- 1..@pages} phx-click="set-page" phx-value-page={page}>
-        <%= page %>
-      </span>
-      <span>next</span>
+    <div class="flex flex-row justify-center space-x-6 pt-8 text-xl font-semibold">
+      <button class="px-2 font-medium">&#8592; prev</button>
+      <button :for={page <- 1..@pages} phx-click="set-page" phx-value-page={page}><%= page %></button>
+      <button class="px-2 font-medium">next &#8594;</button>
     </div>
     """
   end
