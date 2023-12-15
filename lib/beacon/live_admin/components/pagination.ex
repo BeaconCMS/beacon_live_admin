8217b296b7d0972dd109c6f717232b77627783a6
ChristianTovar <tovarchristian21@gmail.com>
2023-12-15 12:21:08 -0500

    Inline tailwind classes by state

--- lib/beacon/live_admin/components/pagination.ex
+++ lib/beacon/live_admin/components/pagination.ex
@@ -4,19 +4,12 @@ defmodule Beacon.LiveAdmin.Components.Pagination do
   def render(assigns) do
     ~H"""
     <div class="flex flex-row justify-center space-x-6 pt-8 text-xl font-semibold">
-      <button phx-click="prev-page" class={"px-2 font-medium #{active_prev(@current_page)}"}>&#8592; prev</button>
-      <button :for={page <- 1..@pages} phx-click="set-page" phx-value-page={page} class={active_page(page, @current_page)}><%= page %></button>
-      <button phx-click="next-page" class={"px-2 font-medium #{active_next(@current_page, @pages)}"}>next &#8594;</button>
+      <button phx-click="prev-page" disabled={@current_page == 1} class="px-2 font-medium disabled:text-gray-400">&#8592; prev</button>
+      <button :for={page <- 1..@pages} phx-click="set-page" phx-value-page={page} class={if @current_page == page, do: "text-indigo-700", else: ""}>
+        <%= page %>
+      </button>
+      <button phx-click="next-page" disabled={@current_page == @pages} class="px-2 font-medium disabled:text-gray-400">next &#8594;</button>
     </div>
     """
   end
-
-  defp active_page(page, page), do: "text-indigo-700"
-  defp active_page(_, _), do: ""
-
-  defp active_prev(1), do: "text-gray-400 disabled"
-  defp active_prev(_), do: ""
-
-  defp active_next(page, page), do: "text-gray-400 disabled"
-  defp active_next(_, _), do: ""
 end
