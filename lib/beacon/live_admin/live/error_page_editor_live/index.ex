059c46b1676fc3a5240dc04ec1542cb33f0f09c4
Leandro Pereira <leandro@leandro.io>
2024-01-31 10:52:19 -0500

    Support LiveView v0.20.3+

--- lib/beacon/live_admin/live/error_page_editor_live/index.ex
+++ lib/beacon/live_admin/live/error_page_editor_live/index.ex
@@ -4,7 +4,7 @@ defmodule Beacon.LiveAdmin.ErrorPageEditorLive.Index do
 
   alias Beacon.LiveAdmin.Content
 
-  on_mount({Beacon.LiveAdmin.Hooks.Authorized, {:error_pages, :index}})
+  on_mount {Beacon.LiveAdmin.Hooks.Authorized, {:error_pages, :index}}
 
   def menu_link(_, :index), do: {:root, "Error Pages"}
 
