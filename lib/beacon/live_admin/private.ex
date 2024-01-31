1fff6e91cdefd3970ff625f6f4b90633ee0d4325
Leandro Pereira <leandro@leandro.io>
2024-01-31 10:59:29 -0500

    Support LiveView v0.20.3+ (#101)
    * Support LiveView v0.20.3+
    
    * revert build hooks for <=0.20.2

--- lib/beacon/live_admin/private.ex
+++ lib/beacon/live_admin/private.ex
@@ -4,6 +4,8 @@ defmodule Beacon.LiveAdmin.Private do
   # Call to private APIs.
   # Those should be handled carefully and not be spread over the codebase.
 
+  @phoenix_live_view_version to_string(Application.spec(:phoenix_live_view)[:vsn])
+
   def mount(params, session, socket) do
     Phoenix.LiveView.Lifecycle.mount(params, session, socket)
   end
@@ -16,7 +18,24 @@ defmodule Beacon.LiveAdmin.Private do
     Module.get_attribute(mod, :phoenix_live_mount)
   end
 
-  def build_on_mount_lifecycle(socket, page_module) do
-    update_in(socket.private.lifecycle.mount, &(&1 ++ Enum.reverse(page_module.on_mount())))
+  # support LiveView up to v0.20.2
+  if Version.compare(@phoenix_live_view_version, "0.20.2") in [:lt, :eq] do
+    def build_on_mount_lifecycle(socket, page_module) do
+      update_in(socket.private.lifecycle.mount, &(&1 ++ Enum.reverse(page_module.on_mount())))
+    end
+  end
+
+  # support LiveView v0.20.3+
+  if Version.compare(@phoenix_live_view_version, "0.20.3") in [:gt, :eq] do
+    def build_on_mount_lifecycle(socket, page_module) do
+      update_in(socket.private.lifecycle.mount, fn hooks ->
+        page_hooks =
+          page_module.on_mount()
+          |> Phoenix.LiveView.Lifecycle.prepare_on_mount!()
+          |> Enum.reverse()
+
+        hooks ++ page_hooks
+      end)
+    end
   end
 end
