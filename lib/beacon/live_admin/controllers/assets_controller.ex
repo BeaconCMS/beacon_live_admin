fdce7f5b93215661aaf75668362b9895848e10d3
Leandro Pereira <leandro@leandro.io>
2024-02-29 18:10:01 -0500

    Add immutable directive to cache-control header

--- lib/beacon/live_admin/controllers/assets_controller.ex
+++ lib/beacon/live_admin/controllers/assets_controller.ex
@@ -33,7 +33,7 @@ defmodule Beacon.LiveAdmin.AssetsController do
 
     conn
     |> put_resp_header("content-type", content_type)
-    |> put_resp_header("cache-control", "public, max-age=31536000")
+    |> put_resp_header("cache-control", "public, max-age=31536000, immutable")
     |> put_private(:plug_skip_csrf_protection, true)
     |> send_resp(200, contents)
     |> halt()
