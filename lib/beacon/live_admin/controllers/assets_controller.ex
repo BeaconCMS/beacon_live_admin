4d37138ca20f8b7ef2e6c647bfdefcefefaca36b
Leandro Pereira <leandro@leandro.io>
2024-03-01 13:27:37 -0500

    add http endpoints to return both site and page stylesheets

--- lib/beacon/live_admin/controllers/assets_controller.ex
+++ lib/beacon/live_admin/controllers/assets_controller.ex
@@ -32,21 +32,46 @@ defmodule Beacon.LiveAdmin.AssetsController do
     :js => Base.encode16(:crypto.hash(:md5, @js), case: :lower)
   }
 
-  def init(asset) when asset in [:css, :js], do: asset
+  def init(asset) when asset in [:css, :css_site, :css_page, :js], do: asset
 
   def call(conn, asset) do
-    {contents, content_type} = contents_and_type(asset)
+    {contents, content_type} = contents_and_type(asset, conn.params)
 
     conn
     |> put_resp_header("content-type", content_type)
-    |> put_resp_header("cache-control", "public, max-age=31536000")
+    |> put_resp_header("cache-control", "public, max-age=31536000, immutable")
     |> put_private(:plug_skip_csrf_protection, true)
     |> send_resp(200, contents)
     |> halt()
   end
 
-  defp contents_and_type(:css), do: {@css, "text/css"}
-  defp contents_and_type(:js), do: {@js, "text/javascript"}
+  defp contents_and_type(:css, _params), do: {@css, "text/css"}
+
+  defp contents_and_type(:css_site, %{"site" => site}) do
+    {Beacon.LiveAdmin.RuntimeCSS.fetch(site, :uncompressed), "text/css"}
+  end
+
+  defp contents_and_type(:css_page, %{"site" => site, "page_id" => page_id}) do
+    css =
+      with %{site: site, template: template} <- Beacon.LiveAdmin.Content.get_page(site, page_id),
+           {:ok, stylesheet} <- Beacon.LiveAdmin.RuntimeCSS.compile(site, template) do
+        stylesheet
+      else
+        {:error, error} ->
+          """
+          /*
+          #{error}
+          /*
+          """
+
+        _ ->
+          "/* failed to generate the page stylesheet */"
+      end
+
+    {css, "text/css"}
+  end
+
+  defp contents_and_type(:js, _params), do: {@js, "text/javascript"}
 
   @doc """
   Returns the current hash for the given `asset`.
--- lib/beacon/live_admin/runtime_css.ex
+++ lib/beacon/live_admin/runtime_css.ex
@@ -5,6 +5,11 @@ defmodule Beacon.LiveAdmin.RuntimeCSS do
     call(site, Beacon.RuntimeCSS, :compile, [site, template])
   end
 
+  def fetch(site, version) when is_binary(site) do
+    site = String.to_existing_atom(site)
+    fetch(site, version)
+  end
+
   def fetch(site, version) when is_atom(site) do
     call(site, Beacon.RuntimeCSS, :fetch, [site, version])
   end
