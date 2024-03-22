bf78b78a744f0fd4a71396a4fbab938d31cf03fd
Leandro Pereira <leandro@leandro.io>
2024-03-22 09:45:45 -0400

    WIP: jit-browser-tailwindcss

--- assets/svelte/components/PageWrapper.svelte
+++ assets/svelte/components/PageWrapper.svelte
@@ -6,18 +6,43 @@
   import { page } from "$lib/stores/page"
   import { pageBaselineCssPath as pageBaselineCssPathStore } from "$lib/stores/pageBaselineCssPath"
   import { pageChunksCssPath as pageChunksCssPathStore } from "$lib/stores/pageChunksCssPath"
+  import { onMount } from "svelte"
+
+  onMount(() => {
+    const pageEl = document.getElementById("page-wrapper-content")
+    console.log(pageEl)
+  })
+
+  const reloadStylesheet = async () => {
+    await import("https://unpkg.com/@mhsdesign/jit-browser-tailwindcss@0.4.0/dist/cdn.min.js")
+
+    // TODO: fetch content from page nodes
+    const content = `<div class="bg-red-500">el</div>`
+
+    const css = await window.jitBrowserTailwindcss(
+      `
+      @tailwind components;
+      @tailwind utilities;
+      `,
+      content,
+    )
+
+    // TODO: apply style to page
+    console.log(css)
+  }
+
+  window.reloadStylesheet = reloadStylesheet
 </script>
 
-<link id="page-baseline-css-target" rel="stylesheet" href={$pageBaselineCssPathStore} />
-<link id="page-chunks-css-target" rel="stylesheet" href={$pageChunksCssPathStore} />
-
-{#each $page.layout.ast as layoutAstNode}
-  <LayoutAstNode node={layoutAstNode}>
-    {#each $page.ast as astNode, index}
-      <PageAstNode node={astNode} nodeId={String(index)} />
-    {/each}
-  </LayoutAstNode>
-{/each}
+<div id="page-wrapper-content">
+  {#each $page.layout.ast as layoutAstNode}
+    <LayoutAstNode node={layoutAstNode}>
+      {#each $page.ast as astNode, index}
+        <PageAstNode node={astNode} nodeId={String(index)} />
+      {/each}
+    </LayoutAstNode>
+  {/each}
+</div>
 
 <style>
   :global([data-selected="true"], [data-highlighted="true"]) {
