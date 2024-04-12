382d104ae9b8f7868d0c6a1e7ad918a8d9b3227c
Miguel Camba <miguel.camba@gmail.com>
2024-04-12 18:00:29 +0200

    Improve level up icon

--- assets/svelte/components/PropertiesSidebar.svelte
+++ assets/svelte/components/PropertiesSidebar.svelte
@@ -134,18 +134,9 @@
               class="absolute opacity-0 invisible right-9 min-w-[100px] bg-amber-100 py-1 px-1.5 rounded text-xs text-medium transition group-hover:opacity-100 group-hover:visible"
               >Up one level</span
             >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="w-6 h-6 hover:text-blue-700 active:text-blue-900"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-blue-700 active:text-blue-900">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
+            </svg> 
           </button>
         {/if}
         <button type="button" class="absolute p-2 top-2 right-1" on:click={() => ($selectedAstElementId = undefined)}>
