98e244738c57e317fad46fb44f080e64c1f997c5
Miguel Camba <miguel.camba@gmail.com>
2024-05-31 21:33:16 +0200

    Resize Content input text to fit more text (#162)
    Closes #152

--- assets/svelte/components/SidebarSection.svelte
+++ assets/svelte/components/SidebarSection.svelte
@@ -188,14 +188,24 @@
               </div>
             </div>
           {:else}
-            <input
-              type="text"
-              class="w-full py-1 px-2 mt-5 bg-slate-100 border-slate-100 rounded-md leading-6 text-sm"
-              {placeholder}
-              value={astNode}
-              on:keydown={handleKeydown}
-              on:change={(e) => updateNodeContents(e, idx)}
-            />
+            {#if large}
+              <textarea
+                class="w-full py-1 px-2 bg-slate-100 border-slate-100 rounded-md leading-6 text-sm"
+                {placeholder}
+                value={astNode}
+                on:keydown={handleKeydown}
+                on:change={(e) => updateNodeContents(e, idx)}
+              ></textarea>       
+            {:else}
+              <input
+                type="text"
+                class="w-full py-1 px-2 mt-5 bg-slate-100 border-slate-100 rounded-md leading-6 text-sm"
+                {placeholder}
+                value={astNode}
+                on:keydown={handleKeydown}
+                on:change={(e) => updateNodeContents(e, idx)}
+              />
+            {/if}
           {/if}
         {/each}
       {/if}
