dfa27d67672f82d118618b8bb317027fa0bd6b10
Leandro Pereira <leandro@leandro.io>
2024-06-20 18:08:06 -0400

    Update beacon

--- lib/beacon/live_admin/beacon_web.ex
+++ lib/beacon/live_admin/beacon_web.ex
@@ -2,8 +2,7 @@ defmodule Beacon.LiveAdmin.BeaconWeb do
   @moduledoc false
   import Beacon.LiveAdmin.Cluster, only: [call: 4]
 
-  def beacon_assigns(site, path_info, query_params) do
-    beacon = call(site, BeaconWeb.BeaconAssigns, :build, [site])
-    call(site, BeaconWeb.BeaconAssigns, :build, [beacon, path_info, query_params])
+  def beacon_assigns(site, page, live_data, path_info, query_params) do
+    call(site, BeaconWeb.BeaconAssigns, :new, [site, page, live_data, path_info, query_params])
   end
 end
