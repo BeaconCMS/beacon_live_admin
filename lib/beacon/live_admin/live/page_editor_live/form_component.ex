318986115d031eb999357be8650c7457f97144d8
ChristianTovar <tovarchristian21@gmail.com>
2024-07-12 12:22:24 -0400

    Allow users switching back to code editor

--- lib/beacon/live_admin/live/page_editor_live/form_component.ex
+++ lib/beacon/live_admin/live/page_editor_live/form_component.ex
@@ -239,7 +239,7 @@ defmodule Beacon.LiveAdmin.PageEditorLive.FormComponent do
           <.button :if={@live_action in [:new, :edit] && @editor == "code" && @page.format == :heex} type="button" phx-click="enable_editor" phx-value-editor="visual" class="uppercase">
             Visual Editor
           </.button>
-          <.button :if={@live_action == :edit && @editor == "visual"} type="button" phx-click="enable_editor" phx-value-editor="code" class="uppercase">Code Editor</.button>
+          <.button :if={@live_action in [:new, :edit] && @editor == "visual"} type="button" phx-click="enable_editor" phx-value-editor="code" class="uppercase">Code Editor</.button>
           <.button :if={@live_action == :new} phx-disable-with="Saving..." form="page-form" class="uppercase">Create Draft Page</.button>
           <.button :if={@live_action == :edit} phx-disable-with="Saving..." form="page-form" class="uppercase">Save Changes</.button>
           <.button :if={@live_action == :edit} phx-click={show_modal("publish-confirm-modal")} phx-target={@myself} class="uppercase">Publish</.button>
