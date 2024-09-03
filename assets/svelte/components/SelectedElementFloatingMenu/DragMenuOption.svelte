9fd784de96f017ce96b88a8c980c8d8d5cfb55b3
Leandro Pereira <leandro@leandro.io>
2024-09-03 12:26:03 -0400

    format

--- assets/svelte/components/SelectedElementFloatingMenu/DragMenuOption.svelte
+++ assets/svelte/components/SelectedElementFloatingMenu/DragMenuOption.svelte
@@ -51,7 +51,7 @@
 
 <script lang="ts">
   import { tick } from "svelte"
-  $: rotated = getDragDirection($selectedDomElement) === 'horizontal'
+  $: rotated = getDragDirection($selectedDomElement) === "horizontal"
 
   let dragHandleElement: HTMLButtonElement
 
@@ -114,9 +114,9 @@
       parent.content.splice(newIndex, 0, selectedAstElement)
       // Update the selectedAstElementId so the same item remains selected
       $page.ast = [...$page.ast]
-      let parts = $selectedAstElementId.split('.')
+      let parts = $selectedAstElementId.split(".")
       parts[parts.length - 1] = newIndex.toString()
-      $selectedAstElementId = parts.join('.')
+      $selectedAstElementId = parts.join(".")
       tick().then(() => initSelectedElementDragMenuPosition($selectedDomElement))
       // Update in the server
       $live.pushEvent("update_page_ast", { id: $page.id, ast: $page.ast })
@@ -320,10 +320,10 @@
     if (dragDirection === "vertical") {
       // Only the drag handle can use CSS variables because it has the `transform` tailwind class
       // CSS variables allow to control translate and rotate independently.
-      dragHandleElement.style.setProperty('--tw-translate-y', `${mouseDiff.y}px`)
+      dragHandleElement.style.setProperty("--tw-translate-y", `${mouseDiff.y}px`)
       ghostElement.style.transform = `translateY(${mouseDiff.y}px)`
     } else {
-      dragHandleElement.style.setProperty('--tw-translate-x', `${mouseDiff.x}px`)
+      dragHandleElement.style.setProperty("--tw-translate-x", `${mouseDiff.x}px`)
       ghostElement.style.transform = `translateX(${mouseDiff.x}px)`
     }
 
@@ -340,7 +340,7 @@
     on:mousedown={handleMousedown}
     class="rounded-full w-6 h-6 flex justify-center items-center absolute bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus-visible:ring-4 focus-visible:ring-blue-200 active:bg-blue-800 transform"
     class:pointer-events-none={$selectedElementMenu?.dragging}
-     class:rotate-90={rotated}
+    class:rotate-90={rotated}
     style={$dragHandleStyle}
   >
     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"
