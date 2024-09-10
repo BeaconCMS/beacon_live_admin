20c8c2a4595385ae37ff0722d737ead376ba5f76
Leandro Pereira <leandro@leandro.io>
2024-09-10 09:32:54 -0400

    [Page Editor] only paginate when table data is available

--- lib/beacon/live_admin/live/page_editor_live/index.ex
+++ lib/beacon/live_admin/live/page_editor_live/index.ex
@@ -14,14 +14,11 @@ defmodule Beacon.LiveAdmin.PageEditorLive.Index do
   end
 
   @impl true
-  def handle_params(params, _uri, socket) do
+  def handle_params(params, _uri, %{assigns: %{beacon_page: beacon_page}} = socket) do
     socket =
       Table.handle_params(socket, params, &Content.count_pages(&1.site, query: params["query"]))
 
-    %{site: site} = socket.assigns.beacon_page
-
-    %{per_page: per_page, current_page: page, query: query, sort_by: sort_by} =
-      socket.assigns.beacon_page.table
+    %{site: site, table: %{per_page: per_page, current_page: page, query: query, sort_by: sort_by}} = beacon_page
 
     pages =
       list_pages(site,
@@ -33,6 +30,8 @@ defmodule Beacon.LiveAdmin.PageEditorLive.Index do
 
     {:noreply, stream(socket, :pages, pages, reset: true)}
   end
+
+  def handle_params(_params, _uri, socket), do: {:noreply, socket}
 
   @impl true
   def render(assigns) do
