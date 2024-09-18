326fa2928b4b721057096d79c65ee8a8bfddbf1d
Chantal Broeren <chantal.broeren@dockyard.com>
2024-09-18 22:44:35 +0200

    Add first tests as an example

--- test/e2e/index.spec.ts
+++ test/e2e/index.spec.ts
@@ -1,8 +1,45 @@
 import { test, expect } from '@playwright/test';
 
 test('renders', async ({ page }) => {
-  await page.goto('/admin');
+    await page.goto('/admin');
 
-  await expect(page).toHaveTitle('Beacon LiveAdmin');
-  await expect(page.locator('h1')).toHaveText('Welcome!');
+    await expect(page).toHaveTitle('Beacon LiveAdmin');
+    await expect(page.locator('h1')).toHaveText('Welcome!');
+});
+
+test('shows the list of available sites', async({ page }) => {
+    const siteName = 'dockyard_com';
+
+    await page.goto('/admin');
+
+    await expect(page.locator('h2#admin-sites')).toHaveText('Sites');
+
+    await expect(page.getByRole('heading', { name: siteName })).toBeVisible();
+});
+
+[
+    { name: 'Layouts', path: 'layouts' },
+    { name: 'Components', path: 'components' },
+    { name: 'Pages', path: 'pages' },
+    { name: 'Live Data', path: 'live_data' },
+    { name: 'Error Pages', path: 'error_pages' },
+    { name: 'Media Library', path: 'media_library' },
+    { name: 'Event Handlers', path: 'events' },
+].forEach(({ name, path }, index) => {
+    // TODO: Update for using a test site
+    const siteName = 'dockyard_com';
+
+    test(`shows link for ${name}`, async ({ page, baseURL }) => {
+        await page.goto('/admin');
+
+        const link = page.getByRole('link').nth(index);
+
+        await expect(link).toBeVisible();
+        await expect(link).toHaveText(name);
+        await expect(link).toHaveAttribute('href', `/admin/${siteName}/${path}`);
+
+        await link.click();
+
+        await expect(page.url()).toBe(`${baseURL}/admin/${siteName}/${path}`);
+    });
 });
