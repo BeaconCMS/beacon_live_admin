b12d908e0602b23d659e84022380348a02f0a6c9
Leandro Pereira <leandro@leandro.io>
2024-09-26 19:48:10 -0400

    Fix index tests: reorder pages
    Changed on main

--- test/e2e/index.spec.ts
+++ test/e2e/index.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from "@playwright/test";
+const { syncLV, attributeMutations } = require("./utils");
 
 test("renders", async ({ page }) => {
   await page.goto("/");
+  await syncLV(page);
 
   await expect(page).toHaveTitle("Beacon LiveAdmin");
   await expect(page.locator("h1")).toHaveText("Welcome!");
@@ -11,6 +13,7 @@ test("shows the list of available sites", async ({ page }) => {
   const siteName = "site_a";
 
   await page.goto("/");
+  await syncLV(page);
 
   await expect(page.locator("h2#admin-sites")).toHaveText("Sites");
 
@@ -18,19 +21,21 @@ test("shows the list of available sites", async ({ page }) => {
 });
 
 [
-  { name: "Layouts", path: "layouts" },
+  { name: "Media Library", path: "media_library" },
   { name: "Components", path: "components" },
+  { name: "Layouts", path: "layouts" },
   { name: "Pages", path: "pages" },
   { name: "Live Data", path: "live_data" },
-  { name: "Error Pages", path: "error_pages" },
-  { name: "Media Library", path: "media_library" },
   { name: "Event Handlers", path: "events" },
+  { name: "Info Handlers", path: "info_handlers" },
+  { name: "Error Pages", path: "error_pages" },
 ].forEach(({ name, path }, index) => {
   // TODO: Update for using a test site
   const siteName = "site_a";
 
   test(`shows link for ${name}`, async ({ page, baseURL }) => {
     await page.goto("/");
+    await syncLV(page);
 
     const link = page.getByRole("link").nth(index);
 
@@ -39,6 +44,7 @@ test("shows the list of available sites", async ({ page }) => {
     await expect(link).toHaveAttribute("href", `/${siteName}/${path}`);
 
     await link.click();
+    await syncLV(page);
 
     await expect(page).toHaveURL(`${baseURL}/${siteName}/${path}`);
   });
