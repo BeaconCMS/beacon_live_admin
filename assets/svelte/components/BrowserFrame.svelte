c2578475d5710cac2135e191a935aa28cc8014df
leandrocp <leandrocp@users.noreply.github.com>
2024-10-03 19:53:19 +0000

    auto format code

--- assets/svelte/components/BrowserFrame.svelte
+++ assets/svelte/components/BrowserFrame.svelte
@@ -9,10 +9,7 @@
 </script>
 
 <div class="flex-1 flex flex-col" data-testid="fake-browser">
-  <div
-    class="bg-gray-50 border-b border-gray-200 border-solid rounded-t-xl h-12 px-3.5 flex"
-    data-testid="address-bar"
-  >
+  <div class="bg-gray-50 border-b border-gray-200 border-solid rounded-t-xl h-12 px-3.5 flex" data-testid="address-bar">
     <div class="py-2">
       <span class="inline-block h-2 w-2 ml-2 rounded-full bg-red-800"></span>
       <span class="inline-block h-2 w-2 ml-2 rounded-full bg-amber-400"></span>
