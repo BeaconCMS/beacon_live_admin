939546f5d0142af6c8ded3b46fdc299ec15307d3
Miguel Camba <miguel.camba@gmail.com>
2024-10-26 00:50:04 +0200

    Add some more sample stuff

--- lib/beacon/live_admin/components/properties_sidebar_component.ex
+++ lib/beacon/live_admin/components/properties_sidebar_component.ex
@@ -37,7 +37,8 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
     assigns =
       assign(assigns,
         selected_ast_element: selected_ast_element,
-        parent_node_id: parent_node_id
+        parent_node_id: parent_node_id,
+        attributes_editable: selected_ast_element["tag"] not in ["eex", "eex_block"]
       )
 
     ~H"""
@@ -49,6 +50,7 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
               <%= if assigns.selected_ast_element_id !== "root" do %>
                 <.svelte name="components/GoToParentButton" class="contents" socket={@socket}/>
               <% end %>
+
               <button type="button" class="absolute p-2 top-2 right-1" phx-click="reset_selection">
                 <span class="sr-only">Close</span>
                 <svg
@@ -65,9 +67,58 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
                 </svg>
               </button>
             </div>
+            <%= if @attributes_editable do %>
+              <.sidebar_section placeholder="Placeholder" name="Attr name" value="Attr value"></.sidebar_section>
+              <.sidebar_section placeholder="Placeholder" name="Attr name 2" value="Attr value 2"></.sidebar_section>
+            <% end %>
           <% end %>
         </div>
       </div>
+    """
+  end
+
+  def sidebar_section(assigns) do
+    ~H"""
+    <section class="p-4 border-b border-b-gray-100 border-solid">
+      <header class="flex items-center text-sm mb-2 font-medium">
+          <%!-- on:click={() => (expanded = !expanded)} --%>
+          <%!-- aria-expanded={expanded} --%>
+        <button
+          type="button"
+          class="w-full flex items-center justify-between gap-x-1 p-1 font-semibold group"
+        >
+          <span>
+            <span class="hover:text-blue-700 active:text-blue-900">
+              <%= @name %>
+            </span>
+            <button type="button" class="ml-4" title="Delete attribute">
+              <span class="hero-trash text-red hover:text-red"></span>
+            </button>
+          </span>
+          <%!-- <span class={expanded ? "" : " [&_path]:origin-center [&_path]:rotate-180"}> --%>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-5 h-5 stroke-slate-500 fill-slate-500 group-hover:stroke-current group-hover:fill-current"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+        </button>
+        <!-- Classes -->
+      </header>
+      <input
+        type="text"
+        class="w-full py-1 px-2 bg-gray-100 border-gray-100 rounded-md leading-6 text-sm"
+        value={@value}
+      />
+    </section>
     """
   end
 end
