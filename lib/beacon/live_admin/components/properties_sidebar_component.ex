d7e59cae639ddc9e9982a23f68962938e47f44d5
Miguel Camba <miguel.camba@gmail.com>
2024-11-08 12:43:30 +0100

    Extract a few components

--- lib/beacon/live_admin/components/properties_sidebar_component.ex
+++ lib/beacon/live_admin/components/properties_sidebar_component.ex
@@ -39,30 +39,29 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
   end
 
   def handle_event("delete_attribute", %{"index" => index}, socket) do
-    Logger.debug("Deleting attribute: #{index}")
     new_attributes = List.delete_at(socket.assigns.new_attributes, String.to_integer(index))
     {:noreply, assign(socket, :new_attributes, new_attributes)}
   end
 
-  def handle_event("update_attribute_name", %{"index" => index, "name" => name}, socket) do
-    Logger.debug("Updating attribute name: #{index} - #{name}")
-    index = String.to_integer(index)
-    new_attributes = Enum.map(socket.assigns.new_attributes, fn
-      {attr, i} when i == index -> %{attr | name: name}
-      attr -> attr
-    end)
-    {:noreply, assign(socket, :new_attributes, new_attributes)}
-  end
+  # def handle_event("update_attribute_name", %{"index" => index, "name" => name}, socket) do
+  #   Logger.debug("Updating attribute name: #{index} - #{name}")
+  #   index = String.to_integer(index)
+  #   new_attributes = Enum.map(socket.assigns.new_attributes, fn
+  #     {attr, i} when i == index -> %{attr | name: name}
+  #     attr -> attr
+  #   end)
+  #   {:noreply, assign(socket, :new_attributes, new_attributes)}
+  # end
 
-  def handle_event("update_attribute_value", %{"index" => index, "value" => value}, socket) do
-    Logger.debug("Updating attribute value: #{index} - #{value}")
-    index = String.to_integer(index)
-    new_attributes = Enum.map(socket.assigns.new_attributes, fn
-      {attr, i} when i == index -> %{attr | value: value}
-      attr -> attr
-    end)
-    {:noreply, assign(socket, :new_attributes, new_attributes)}
-  end
+  # def handle_event("update_attribute_value", %{"index" => index, "value" => value}, socket) do
+  #   Logger.debug("Updating attribute value: #{index} - #{value}")
+  #   index = String.to_integer(index)
+  #   new_attributes = Enum.map(socket.assigns.new_attributes, fn
+  #     {attr, i} when i == index -> %{attr | value: value}
+  #     attr -> attr
+  #   end)
+  #   {:noreply, assign(socket, :new_attributes, new_attributes)}
+  # end
 
   @spec render(
           atom()
@@ -89,25 +88,8 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
           <%= if @selected_ast_element do %>
             <div class="border-b text-lg font-medium leading-5 p-4 relative">
               <%= @selected_ast_element["tag"] %>
-              <%= if assigns.selected_ast_element_id !== "root" do %>
-                <.svelte name="components/GoToParentButton" class="contents" socket={@socket}/>
-              <% end %>
-
-              <button type="button" class="absolute p-2 top-2 right-1" phx-click="reset_selection">
-                <span class="sr-only">Close</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  class="w-6 h-6 hover:text-blue-700 active:text-blue-900"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
+              <.go_to_parent_button selected_ast_element_id={assigns.selected_ast_element_id} socket={@socket}/>
+              <.close_button />
             </div>
 
             <%= if @attributes_editable do %>
@@ -130,6 +112,34 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
           <% end %>
         </div>
       </div>
+    """
+  end
+
+  def close_button(assigns) do
+    ~H"""
+      <button type="button" class="absolute p-2 top-2 right-1" phx-click="reset_selection">
+        <span class="sr-only">Close</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6 hover:text-blue-700 active:text-blue-900"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+    """
+  end
+
+  def go_to_parent_button(assigns) do
+    ~H"""
+      <%= if @selected_ast_element_id !== "root" do %>
+        <.svelte name="components/GoToParentButton" class="contents" socket={@socket}/>
+      <% end %>
     """
   end
 end
