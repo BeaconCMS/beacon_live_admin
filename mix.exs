d88dfbd0d8397fe273aa4490898aa0cb47f9b26e
Leandro Pereira <leandro@leandro.io>
2024-11-14 09:26:58 -0500

    v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Beacon.LiveAdmin.MixProject do
   use Mix.Project
 
-  @version "0.1.2-dev"
+  @version "0.2.0"
   @dev? String.ends_with?(@version, "-dev")
   @source_url "https://github.com/BeaconCMS/beacon_live_admin"
   @homepage_url "https://beaconcms.org"
