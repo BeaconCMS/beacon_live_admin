9d8a27c437ddf46755aa1d9c78f40d82664d7f7f
Miguel Camba <miguel.camba@gmail.com>
2024-11-28 23:41:43 +0100

    Save progress

--- lib/beacon/live_admin/components/visual_editor/key_value_control.ex
+++ lib/beacon/live_admin/components/visual_editor/key_value_control.ex
@@ -6,10 +6,12 @@ defmodule Beacon.LiveAdmin.VisualEditor.KeyValueControl do
   # FIXME: create functions components to reuse shared styles (currently defined in PropertiesSidebarSectionComponent)
   def render(assigns) do
     ~H"""
-    <div>
-      <.form :let={f} for={@form} as={@as} phx-target={@myself} phx-change="update">
-        <.input field={f[:name]} label="Name" phx-debounce="blur" />
-        <.input field={f[:value]} label="Value" phx-debounce="blur" />
+    <div id={@id}>
+      <.form :let={f} for={@form} as={@as} phx-target={@myself} phx-blur="update">
+        <.input field={f[:name]} label="Name" />
+        <%!-- phx-debounce="blur" /> --%>
+        <.input field={f[:value]} label="Value" phx-debounce="update"/>
+        <%!-- phx-debounce="blur" /> --%>
       </.form>
     </div>
     """
@@ -20,13 +22,13 @@ defmodule Beacon.LiveAdmin.VisualEditor.KeyValueControl do
     {:ok, assign_form(socket, "", "")}
   end
 
-  def update(%{index: index, element: element}, socket) do
+  def update(%{index: index, element: element, id: id}, socket) do
     as = String.to_atom("kv_#{index}")
     path = Map.get(element, "path", "")
 
     {:ok,
      socket
-     |> assign(as: as, path: path)
+     |> assign(as: as, path: path, id: id)
      |> assign_form("", "")}
   end
 
@@ -37,10 +39,12 @@ defmodule Beacon.LiveAdmin.VisualEditor.KeyValueControl do
   # TODO: validate k/v is valid
   # TODO: required `value` before sending? to avoid invalid html
   def handle_event("update", attrs, socket) do
-    %{"name" => name, "value" => value} = Map.get(attrs, Atom.to_string(socket.assigns.as), %{})
-    %{path: path} = socket.assigns
-    send(self(), {:updated_element, %{path: path, attrs: %{name => value}}})
-    {:noreply, assign_form(socket, name, value)}
+    dbg(attrs)
+    # %{"name" => name, "value" => value} = Map.get(attrs, Atom.to_string(socket.assigns.as), %{})
+    # %{path: path} = socket.assigns
+    # send(self(), {:updated_element, %{path: path, attrs: %{name => value}}})
+    # {:noreply, assign_form(socket, name, value)}
+    {:noreply, socket}
   end
 
   defp assign_form(socket, name, value) do
