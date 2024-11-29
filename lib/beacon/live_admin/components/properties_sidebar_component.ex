747721d0232dc23056797e2279cee0de2a80235c
Leandro Pereira <leandro@leandro.io>
2024-11-29 13:21:40 -0500

    more fixes and optimizations

--- lib/beacon/live_admin/components/properties_sidebar_component.ex
+++ lib/beacon/live_admin/components/properties_sidebar_component.ex
@@ -6,22 +6,32 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
   alias Beacon.LiveAdmin.VisualEditor.OpacityControl
   alias Beacon.LiveAdmin.VisualEditor.KeyValueControl
 
-  def update(assigns, socket) do
-    {_, selected_ast_element} =
-      assigns.page.ast
-      |> VisualEditor.find_element(assigns.selected_ast_element_id)
-      |> Map.pop("content")
+  def update(%{selected_ast_element_id: nil} = assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_new(:new_attributes, fn -> [] end)
+     |> assign(selected_element: nil)}
+  end
 
-    selected_ast_element = Map.put(selected_ast_element, "path", assigns.selected_ast_element_id)
+  def update(assigns, socket) do
+    %{page: %{ast: page}, selected_ast_element_id: selected_ast_element_id} = assigns
+
+    selected_element =
+      case VisualEditor.find_element(page, selected_ast_element_id) do
+        nil ->
+          nil
+
+        element ->
+          {_, selected_element} = Map.pop(element, "content")
+          Map.put(selected_element, "path", selected_ast_element_id)
+      end
 
     {:ok,
      socket
      |> assign(assigns)
      |> assign_new(:new_attributes, fn -> [] end)
-     |> assign(
-       selected_ast_element: selected_ast_element,
-       attributes_editable: VisualEditor.element_editable?(selected_ast_element)
-     )}
+     |> assign(selected_element: selected_element)}
   end
 
   def handle_event("add_attribute", _params, socket) do
@@ -42,32 +52,32 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
   def render(assigns) do
     ~H"""
     <div class="mt-4 w-64 bg-white" data-testid="right-sidebar">
-      <div class="sticky top-0 overflow-y-auto h-screen">
-        <%= if @selected_ast_element_id do %>
-          <div class="border-b text-lg font-medium leading-5 p-4 relative">
-            <%= @selected_ast_element["tag"] %>
-            <.go_to_parent_button selected_ast_element_id={@selected_ast_element_id} socket={@socket} />
-            <.close_button />
-          </div>
+      <div :if={@selected_element} class="sticky top-0 overflow-y-auto h-screen">
+        <div class="border-b text-lg font-medium leading-5 p-4 relative">
+          <%= @selected_element["tag"] %>
+          <.svelte :if={@selected_element["path"] !== "root"} name="components/GoToParentButton" class="contents" socket={@socket} />
+          <%!-- <.close_button /> --%>
+        </div>
 
-          <%= if @attributes_editable do %>
-            <.live_component module={ClassControl} id="control-class" element={@selected_ast_element} />
-            <.live_component module={OpacityControl} id="control-opacity" element={@selected_ast_element} />
-            <%= for index <- @new_attributes do %>
-              <.live_component module={KeyValueControl} id={"control-key-value-#{@selected_ast_element_id}-idx-#{index}"} element={@selected_ast_element} index={index} />
-            <% end %>
+        <%= if VisualEditor.element_editable?(@selected_element) do %>
+          <.live_component module={ClassControl} id="control-class" element={@selected_element} />
+          <.live_component module={OpacityControl} id="control-opacity" element={@selected_element} />
+          <%= for index <- @new_attributes do %>
+            <.live_component module={KeyValueControl} id={"control-key-value-#{@selected_element["path"]}-idx-#{index}"} element={@selected_element} index={index} />
           <% end %>
-          <div class="p-4">
-            <button type="button" class="bg-blue-500 hover:bg-blue-700 active:bg-blue-800 text-white font-bold py-2 px-4 rounded outline-2 w-full" phx-click="add_attribute" phx-target={@myself}>
-              + Add attribute
-            </button>
-          </div>
         <% end %>
+        <div class="p-4">
+          <button type="button" class="bg-blue-500 hover:bg-blue-700 active:bg-blue-800 text-white font-bold py-2 px-4 rounded outline-2 w-full" phx-click="add_attribute" phx-target={@myself}>
+            + Add attribute
+          </button>
+        </div>
       </div>
     </div>
     """
   end
 
+  # FIXME: implement event "reset_selection"
+  # FIXME: use heroicons instead of <svg>
   defp close_button(assigns) do
     ~H"""
     <button type="button" class="absolute p-2 top-2 right-1" phx-click="reset_selection">
@@ -80,14 +90,6 @@ defmodule Beacon.LiveAdmin.PropertiesSidebarComponent do
         />
       </svg>
     </button>
-    """
-  end
-
-  defp go_to_parent_button(assigns) do
-    ~H"""
-    <%= if @selected_ast_element_id !== "root" do %>
-      <.svelte name="components/GoToParentButton" class="contents" socket={@socket} />
-    <% end %>
     """
   end
 end
