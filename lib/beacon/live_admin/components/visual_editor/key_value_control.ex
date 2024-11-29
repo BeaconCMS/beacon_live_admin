fb8ef0e5059c7be28a90da316701ee5c79fdb11a
Miguel Camba <miguel.camba@gmail.com>
2024-11-29 21:02:53 +0100

    Almost working

--- lib/beacon/live_admin/components/visual_editor/key_value_control.ex
+++ lib/beacon/live_admin/components/visual_editor/key_value_control.ex
@@ -7,72 +7,65 @@ defmodule Beacon.LiveAdmin.VisualEditor.KeyValueControl do
   def render(assigns) do
     ~H"""
     <section id={@id} class="p-4 border-b border-b-gray-100 border-solid">
-      <.form :let={f} for={@form} as={@as} phx-target={@myself} phx-blur="update">
-        <.input
-          class="w-full py-1 px-2 bg-gray-100 border-gray-100 rounded-md leading-6 text-sm"
-          field={f[:name]}
-          label="Name"
+      <form phx-change="update" phx-target={@myself}>
+        <input
+          class={"#{if !@edit_name, do: "hidden"} w-full py-1 px-2 bg-gray-100 border-gray-100 rounded-md leading-6 text-sm"}
+          placeholder="Name"
+          name="name"
+          value={@name}
           phx-blur="name_blur"
           phx-target={@myself}/>
-        <.input
-          class="w-full py-1 px-2 bg-gray-100 border-gray-100 rounded-md leading-6 text-sm"
-          field={f[:value]}
-          label="Value"
-          phx-blur="value_blur"
-           phx-target={@myself}/>
-      </.form>
+          <%= if !@edit_name do %>
+            <label class="mb-2 block font-medium capitalize text-sm/5 text-[#304254]">
+              <%= @name %>
+            </label>
+          <% end %>
+        <div class="pt-3">
+          <input
+            class="w-full py-1 px-2 bg-gray-100 border-gray-100 rounded-md leading-6 text-sm"
+            placeholder="Value"
+            name="value"
+            value={@value}
+            phx-blur="value_blur"
+            phx-target={@myself}/>
+        </div>
+      </form>
     </section>
     """
   end
 
   # FIXME: avoid remount to preserve state
   def mount(socket) do
-    {:ok, assign_form(socket, "", "")}
-  end
-
-  def update(%{index: index, element: element, id: id}, socket) do
-    as = String.to_atom("kv_#{index}")
-    path = Map.get(element, "path", "")
-
     {:ok,
-     socket
-     |> assign(as: as, path: path, id: id)
-     |> assign_form("", "")}
+      socket
+      |> assign(edit_name: true, name: "", value: "")}
   end
 
   def update(assigns, socket) do
     {:ok, assign(socket, assigns)}
   end
 
-  def handle_event("name_blur", attrs, socket) do
-    Logger.debug("##################################")
-    Logger.debug("########### name_blur ###############")
-    Logger.debug("##################################")
-    dbg(attrs)
-    {:noreply, socket}
+  def handle_event("name_blur", _, socket) do
+    %{name: name} = socket.assigns
+    {:noreply, socket |> assign(:edit_name, name == "")}
   end
 
-  def handle_event("value_blur", attrs, socket) do
-    Logger.debug("##################################")
+  def handle_event("value_blur", _, socket) do
     Logger.debug("########### value_blur ###############")
-    Logger.debug("##################################")
-    dbg(attrs)
+    %{name: name, value: value} = socket.assigns
+    if value == "" do
+      dbg(socket.assigns.element)
+      send(self(), {:updated_element, {socket.assigns.element["path"], %{"attrs" => %{name => value}}}})
+    end
     {:noreply, socket}
   end
 
-  # TODO: validate k/v is valid
-  # TODO: required `value` before sending? to avoid invalid html
   def handle_event("update", attrs, socket) do
     dbg(attrs)
-    # %{"name" => name, "value" => value} = Map.get(attrs, Atom.to_string(socket.assigns.as), %{})
-    # %{path: path} = socket.assigns
-    # send(self(), {:updated_element, {socket.assigns.element["path"], %{"attrs" => %{name => value}}}})
-    # {:noreply, assign_form(socket, name, value)}
-    {:noreply, socket}
-  end
-
-  defp assign_form(socket, name, value) do
-    form = to_form(%{"name" => name, "value" => value})
-    assign(socket, form: form)
+    %{ "name" => name, "value" => value} = attrs
+    {:noreply,
+      socket
+      |> assign(name: name, value: value)
+    }
   end
 end
