ca91d3557b418b5f9254a92b84484adfcc41dacf
APB9785 <74077809+APB9785@users.noreply.github.com>
2024-12-13 12:31:02 -0600

    fix warning

--- lib/beacon/live_admin/live/page_editor_live/element_selection.ex
+++ lib/beacon/live_admin/live/page_editor_live/element_selection.ex
@@ -1,6 +1,7 @@
 defmodule Beacon.LiveAdmin.PageEditorLive.ElementSelection do
   @moduledoc false
-  use Beacon.LiveAdmin.PageBuilder
+  import Phoenix.Component, only: [assign: 2]
+  import Phoenix.LiveView, only: [send_update: 3]
 
   def select_element(path, socket) do
     {:noreply, assign(socket, selected_element_path: path)}
