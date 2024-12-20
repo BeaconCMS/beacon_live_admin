73f5c91fc17972b1aa6ef2401117031772b7b00c
Leandro Pereira <leandro@leandro.io>
2024-12-20 12:29:39 -0500

    fix: beacon assigns call
    ref: https://github.com/BeaconCMS/beacon/pull/708

--- lib/beacon/live_admin/client/beacon_assigns.ex
+++ lib/beacon/live_admin/client/beacon_assigns.ex
@@ -3,7 +3,7 @@ defmodule Beacon.LiveAdmin.Client.BeaconAssigns do
 
   import Beacon.LiveAdmin.Cluster, only: [call: 4]
 
-  def new(site, page, live_data, path_info, query_params) do
-    call(site, Beacon.Web.BeaconAssigns, :new, [site, page, live_data, path_info, query_params])
+  def new(site, page, metadata \\ []) do
+    call(site, Beacon.Web.BeaconAssigns, :new, [page, metadata])
   end
 end
