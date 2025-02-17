18c897b0345d9d65b9adfa453a25e2abeafa2745
Miguel Camba <miguel.camba@gmail.com>
2025-02-17 21:21:13 +0100

    Reset amount when unit is amount-less

--- lib/beacon/live_admin/components/visual_editor/components/input_with_units.ex
+++ lib/beacon/live_admin/components/visual_editor/components/input_with_units.ex
@@ -10,9 +10,9 @@ defmodule Beacon.LiveAdmin.VisualEditor.Components.InputWithUnits do
 
   def input_with_units(assigns) do
     assigns = assigns
-      |> assign(:is_custom_unit?, Enum.member?(assigns.units, assigns.value_unit))
+      |> assign(:is_custom_unit?, Enum.member?(assigns.units, assigns.value_unit) || assigns.value_unit == "")
 
-    ~H"""
+      ~H"""
     <div class="relative w-full flex bg-gray-100 border rounded focus-within:ring-2 focus-within:ring-blue-500">
       <input
         type="text"
